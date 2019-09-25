b9bf8136ee55d5ffc196b971ba02b6d8ff9cf877
rahmat23 <44376204+rahmatullahahmadi23@users.noreply.github.com>
2019-09-25 13:24:40 +0200

    Update exer1.php

--- formatPHP/exer1.php
+++ formatPHP/exer1.php
@@ -1,9 +1,9 @@
 <?php
-$nombre_de_lignes = 0;
+$lignes = 0;
 
-while ($nombre_de_lignes < 0)
+while ($lignes < 0)
 {
     echo 'Je ne dois pas regarder les mouches voler quand j\'apprends le PHP.<br />';
-    $nombre_de_lignes++; // $nombre_de_lignes = $nombre_de_lignes + 1
+    $lignes++; // $lignes = $lignes + 1
 }
 ?>
