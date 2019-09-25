a5d54254af8ee0fec9296138e80091318ae46f8f
rahmat23 <44376204+rahmatullahahmadi23@users.noreply.github.com>
2019-09-25 14:26:12 +0200

    Update exer1.php

--- formatPHP/exer1.php
+++ formatPHP/exer1.php
@@ -1,6 +1,6 @@
 <?php
 $lignes = 0;
-$output = 'Je ne dois pas regarder les mouches voler quand j\'apprends le PHP'.'\n';
+$output = 'Je ne dois pas regarder les mouches voler quand j\'apprends le PHP'."\n";
 while ($lignes < 0)
 {
     print $output;
