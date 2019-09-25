be49746abea63a82c3dc897651181cd4ea1f40b0
rahmat23 <44376204+rahmatullahahmadi23@users.noreply.github.com>
2019-09-25 14:01:25 +0200

    Update exer1-test.php

--- formatPHP/exer1-test.php
+++ formatPHP/exer1-test.php
@@ -1,13 +1,12 @@
 <?php
     include 'exer1.php';
-    function test() {
-        if ($lignes() ===  10 ) {
+        if ($lignes ===  10 ) {
             echo("TECHIO> success true \r\n");
         } 
         else {
             echo("TECHIO> success false  \r\n");
             echo("TECHIO> message --channel Oops! Try Again 🐞 \r\n");
         }
-    }
+    
    
 ?>
